c449b28d248d9aa62499b437db96c7c0f27cb8ae
Sonny To <sto@beta.fpmsoft.co>
2017-07-27 10:58:48 -0400

    upgrade clojure, clojurescript, and datascript

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/clojurescript "1.9.562"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.671"]
                  [org.clojure/tools.logging "0.3.1"]
                  
                  [mount "0.1.10"]
                  ;;[com.datomic/datomic-pro "0.9.5407" :exclusions [org.slf4j/log4j-over-slf4j org.slf4j/slf4j-nop joda-time com.google.guava/guava]]
                  [datomic-schema "1.3.0"]
-                 [datascript "0.15.4"]
+                 [datascript "0.16.1"]
                  [com.kaicode/wocket "0.1.3-SNAPSHOT"]
                  [com.kaicode/mercury "0.1.2-SNAPSHOT"]]
   :target-path "target/%s")
