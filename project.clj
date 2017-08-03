321dd9b40a2dcdc40a62dd0f5b319da250ab7a05
Sonny To <sto@beta.fpmsoft.co>
2017-08-03 17:31:19 -0400

    fixed unbalanced []

--- project.clj
+++ project.clj
@@ -14,6 +14,6 @@
                  [datascript "0.16.1"]
                  [com.kaicode/wocket "0.1.3-SNAPSHOT"]
                  [com.kaicode/mercury "0.1.2-SNAPSHOT"]
-                 [com.kaicode/tily "0.1.6-SNAPSHOT"]]
+                 [com.kaicode/tily "0.1.6-SNAPSHOT"]
                  [io.replikativ/konserve "0.4.10"]]
   :target-path "target/%s")
